35c9539e05c1de99fd2e88016278e1367feb5ec2
Julian N <ingsistemasjulian@gmail.com>
2022-03-13 11:22:26 -0500

    112. Menu lateral personalizado parte 1 - Se agrega imagen avatar en el menu

--- src/navigator/MenuLateral.tsx
+++ src/navigator/MenuLateral.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {
   createDrawerNavigator,
   DrawerContentComponentProps,
-  DrawerContentOptions,
   DrawerContentScrollView
 } from '@react-navigation/drawer';
 
